2a1df2a39da233eb866b4a8b7e2c04f785ab70d8
tyz2019 <donal.tong@gmail.com>
2022-10-14 13:21:23 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  //height: 100%;
+  height: 100%;
 }
 
 #nav {
--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import OfdView from "ofd-view";
+import 'ofd-view/lib/viewer.css'
 Vue.config.productionTip = false
-
+Vue.use(OfdView)
 new Vue({
   router,
   store,
