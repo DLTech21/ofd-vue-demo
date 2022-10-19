7e0809c286bb8f13b30a7d9d4374f85daa942c62
tyz2019 <donal.tong@gmail.com>
2022-10-19 15:19:30 +0800

    ofdview更新

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height: 100%">
-    <ofd-view @configCallback="configCallback" v-if="isOFD"></ofd-view>
+    <ofd-view @ofdViewReady="ofdViewReady" v-if="isOFD"></ofd-view>
     <pdf-view :url="pdfUrl" v-else></pdf-view>
   </div>
 
@@ -14,7 +14,7 @@ import {
 export default {
   data () {
      return {
-       isOFD: false,
+       isOFD: true,
        pdfUrl: 'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf',
        ofdUrl: 'https://gitee.com/ofdrw/ofdrw/raw/master/ofdrw-converter/src/test/resources/999.ofd'
      }
@@ -27,7 +27,7 @@ export default {
   },
 
   methods: {
-    configCallback(config) {
+    ofdViewReady(config) {
       openOFDViewer(config)
       // // 打开url方式
       openOFD(this.ofdUrl)
