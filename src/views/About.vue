29dec5b6e57647060351e53f0c360de4afdb9103
tyz2019 <donal.tong@gmail.com>
2022-10-21 10:19:57 +0800

    更新版本

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,21 @@
 <template>
   <div style="height: 100%">
-    <ofd-view :url="ofdUrl" v-if="isOFD"></ofd-view>
+    <ofd-view :file="ofdUrl" :mem="mem" v-if="isOFD"></ofd-view>
     <pdf-view v-else :url="pdfUrl" :stamps="stamps" @viewready="pdfViewReady"></pdf-view>
   </div>
 
 </template>
 <script>
-
+const ofdJs = require('ofd.js')
 export default {
   data () {
      return {
-       isOFD: false,
+       isOFD: true,
        pdfUrl: 'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf',
-       ofdUrl: 'https://gitee.com/ofdrw/ofdrw/raw/master/ofdrw-converter/src/test/resources/999.ofd',
+       // ofdUrl: 'https://gitee.com/ofdrw/ofdrw/raw/master/ofdrw-converter/src/test/resources/999.ofd',//线上
+       ofdUrl: '999.ofd',
        stamps: [],
+       mem: ofdJs
      }
   },
 
