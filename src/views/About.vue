0474c356af2b72445751c8df44c6a76e82639f3e
tyz2019 <2238211+DLTech21@users.noreply.github.com>
2023-05-11 11:43:57 +0800

    1

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,15 @@
 <template>
-    <ofd-view :file="ofdUrl" :mem="mem" :stamps="stamps" :sidebar-force-open="false"
-              :signature-viewer-force-check="true" v-if="isOFD"
+    <ofd-view :file="ofdUrl"  :sidebar-force-open="false"
+              :signature-viewer-force-check="true"
               ></ofd-view>
 </template>
 <script>
-const ofdJs = require('parser_x.js')
 export default {
   data () {
      return {
        isOFD: true,
        ofdUrl: '999.ofd',
        stamps: [],
-       mem: ofdJs
      }
   },
 
